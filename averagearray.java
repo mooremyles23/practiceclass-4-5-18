d3e37e2cc6990bf47c553e0f32a92c28a632887d
Moore <mlmoore.2017@mymail.jcsu.edu>
2018-04-05 15:08:32 -0400

    first commit

---
+++ averagearray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class averagearray {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner Scanner= new Scanner(System.in);
+		System.out.print("Pick 5 numbers for the array");
+		
+		int a,b,c,d,e;
+
+	        double avg;
+
+	        a=Scanner.nextInt();
+	        b=Scanner.nextInt();
+	        c=Scanner.nextInt();
+	        d=Scanner.nextInt();
+	        e=Scanner.nextInt();;
+	        avg=(a+b+c+d+e)/5;
+
+	
+		    
+		    System.out.println(avg);
+		
+		
+		
+		
+		
+		
+		
+}}
+
+
